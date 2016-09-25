ef09d20db316cec612290f12d6027c2a33c9cf57
liuxuanqiang <lxq.apk@gmail.com>
2016-09-26 02:19:52 +0800

    0.0.1

---
+++ pages/cart/index.js
@@ -0,0 +1,8 @@
+//获取应用实例
+var app = getApp()
+Page( {
+  data: {
+    cartImg: '../../images/cart-null.png',
+    tipWords: '购物车空空如也'
+  }
+})
---
+++ pages/classify/index.js
@@ -0,0 +1,6 @@
+var app = getApp()
+Page({
+    data: {
+        content: '这是分类页面'
+    }
+})
