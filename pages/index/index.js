94ab35cc0d201679bf224d78e9c712f840f29d07
liuxuanqiang <lxq.apk@gmail.com>
2016-09-26 13:59:28 +0800

    增加了商品详情页

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,77 +1,77 @@
 //获取应用实例
 var app = getApp()
-Page( {
-  data: {
-    indicatorDots: true,
-    vertical: false,
-    autoplay: true,
-    interval: 3000,
-    duration: 1200,
-  },
+Page({
+    data: {
+        indicatorDots: true,
+        vertical: false,
+        autoplay: true,
+        interval: 3000,
+        duration: 1200,
+    },
 
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo( {
-      url: '../logs/logs'
-    })
-  },
-  swiperchange: function(e) {
-    //FIXME: 当前页码
-    //console.log(e.detail.current)
-  },
+    //事件处理函数
+    bindViewTap: function() {
+        wx.navigateTo({
+            url: '../logs/logs'
+        })
+    },
+    swiperchange: function(e) {
+        //FIXME: 当前页码
+        //console.log(e.detail.current)
+    },
 
-  onLoad: function() {
-    console.log( 'onLoad' )
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo( function( userInfo ) {
-      //更新数据
-      that.setData( {
-        userInfo: userInfo
-      })
-    })
+    onLoad: function() {
+        console.log('onLoad')
+        var that = this
+            //调用应用实例的方法获取全局数据
+        app.getUserInfo(function(userInfo) {
+            //更新数据
+            that.setData({
+                userInfo: userInfo
+            })
+        })
 
-    //sliderList
-    wx.request({
-      url: 'http://huanqiuxiaozhen.com/wemall/slider/list',
-      method: 'GET',
-      data: {},
-      header: {
-        'Accept': 'application/json'
-      },
-      success: function(res) {
-        //console.log(res.data)
-        that.data.images = res.data
-      }
-    })
+        //sliderList
+        wx.request({
+            url: 'http://huanqiuxiaozhen.com/wemall/slider/list',
+            method: 'GET',
+            data: {},
+            header: {
+                'Accept': 'application/json'
+            },
+            success: function(res) {
+                //console.log(res.data)
+                that.data.images = res.data
+            }
+        })
 
-    //venuesList
-    wx.request({
-      url: 'http://huanqiuxiaozhen.com/wemall/venues/venuesList',
-      method: 'GET',
-      data: {},
-      header: {
-        'Accept': 'application/json'
-      },
-      success: function(res) {
-        //console.log(res.data.data)
-        that.data.venuesItems = res.data.data
-      }
-    })
+        //venuesList
+        wx.request({
+            url: 'http://huanqiuxiaozhen.com/wemall/venues/venuesList',
+            method: 'GET',
+            data: {},
+            header: {
+                'Accept': 'application/json'
+            },
+            success: function(res) {
+                //console.log(res.data.data)
+                that.data.venuesItems = res.data.data
+            }
+        })
 
-    //choiceList
-    wx.request({
-      url: 'http://huanqiuxiaozhen.com/wemall/goods/choiceList',
-      method: 'GET',
-      data: {},
-      header: {
-        'Accept': 'application/json'
-      },
-      success: function(res) {
-        //console.log(res.data.data.dataList)
-        that.data.choiceItems = res.data.data.dataList
-      }
-    })
+        //choiceList
+        wx.request({
+            url: 'http://huanqiuxiaozhen.com/wemall/goods/choiceList',
+            method: 'GET',
+            data: {},
+            header: {
+                'Accept': 'application/json'
+            },
+            success: function(res) {
+                //console.log(res.data.data.dataList)
+                that.data.choiceItems = res.data.data.dataList
+            }
+        })
 
-  }
+    }
 })
