fa62b22ea3163ee89e10fa4e2ea8060d43314bfd
liuxuanqiang <lxq.apk@gmail.com>
2016-09-27 10:24:35 +0800

    Update 'my' page.

--- pages/my/index.js
+++ pages/my/index.js
@@ -1,12 +1,30 @@
-//index.js
 //获取应用实例
 var app = getApp()
 Page( {
   data: {
     userInfo: {},
-    projectSource: 'github.com/liuxuanqiang'
+    projectSource: 'https://github.com/liuxuanqiang/wechat-weapp-mall',
+    userListInfo: [ {
+      icon: '../../images/iconfont-dingdan.png',
+      text: '我的订单'
+    }, {
+        icon: '../../images/iconfont-card.png',
+        text: '我的代金券'
+      }, {
+        icon: '../../images/iconfont-icontuan.png',
+        text: '我的拼团'
+      }, {
+        icon: '../../images/iconfont-shouhuodizhi.png',
+        text: '收货地址管理'
+      }, {
+        icon: '../../images/iconfont-kefu.png',
+        text: '联系客服'
+      }, {
+        icon: '../../images/iconfont-help.png',
+        text: '常见问题'
+      }]
   },
-  
+
   onLoad: function() {
     var that = this
     //调用应用实例的方法获取全局数据
